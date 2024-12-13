1f62ba6a01e08c8fb61e387c6b38907831dc0e67
Mohit76655 <mohit76655@gmail.com>
2024-12-14 01:14:25 +0530

    fix 104

--- Land_in/client/src/components/UploadImage/UploadImage.jsx
+++ Land_in/client/src/components/UploadImage/UploadImage.jsx
@@ -19,8 +19,8 @@ const UploadImage = ({
     cloudinaryRef.current = window.cloudinary;
     widgetRef.current = cloudinaryRef.current.createUploadWidget(
       {
-        cloudName: "dcdhklrjc",
-        uploadPreset: "vx0dyjgc",
+        cloudName: "dtehd1jgs",
+        uploadPreset: "land_inxyz42",
         maxFiles: 1,
       },
       (err, result) => {
