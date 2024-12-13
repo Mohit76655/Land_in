6e9bbe0d343975247a77ec9bfcfcf63d62f2a87b
Mohit76655 <mohit76655@gmail.com>
2024-12-13 21:15:59 +0530

    99% done

--- Land_in/client/src/main.jsx
+++ Land_in/client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain="dev-fcgzs4kgouncxmyb.us.auth0.com"
       clientId="1aMs72WA2N64hOHIeU4xa74xEgxPSAVG"
       authorizationParams={{
-        redirect_uri: "http://localhost:5173",
+        redirect_uri: "https://land-in-pi.vercel.app/",
       }}
       audience="http://localhost:8000"
       scope="openid profile email"
