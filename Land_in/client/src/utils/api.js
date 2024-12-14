4a71fa137807e9449f113481c12066e3d3391adf
Mohit76655 <mohit76655@gmail.com>
2024-12-14 22:53:19 +0530

    fix 112

--- Land_in/client/src/utils/api.js
+++ Land_in/client/src/utils/api.js
@@ -170,16 +170,16 @@ export const createResidency = async (data, token) => {
   try {
     const res = await api.post(
       `/residency/create`,
-      data,  // Send data directly
+      {
+        data
+      },
       {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       }
     )
-    return res.data;  // Return response data
   } catch (error) {
-    console.error('Error creating residency:', error.response || error);
-    throw new Error(error.response ? error.response.data : error.message);  // Provide error message to the caller
+    throw error
   }
 }
