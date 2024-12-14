59050b605ea9ca93c9941da8999d05bdc9222191
Mohit76655 <mohit76655@gmail.com>
2024-12-14 17:17:01 +0530

    fix 108

--- Land_in/server/index.js
+++ Land_in/server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({ origin: 'https://land-in.vercel.app', credentials: true }));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
